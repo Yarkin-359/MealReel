b626d8d00281e01cfdb731c3e9c473946d2b6fb7
Utku Müjde <utkumujde01@gmail.com>
2024-07-31 21:34:11 +0300

    Added the main screen, customer profile and owner profile files.
    Added the  fxml and java files for the main screen, customer profile and owner profile pages.

--- View/ProfileCustomer.java
+++ View/ProfileCustomer.java
@@ -5,7 +5,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainScreen extends Application {
+public class ProfileCustomer extends Application {
     public void start(Stage primaryStage) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("ProfileCustomer.fxml"));
--- View/ProfileOwner.java
+++ View/ProfileOwner.java
@@ -5,12 +5,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainScreen extends Application {
+public class ProfileOwner extends Application {
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("ProfileCustomer.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("ProfileOwner.fxml"));
             Scene scene = new Scene(root);
-            primaryStage.setTitle("Customer Profile");
+            primaryStage.setTitle("Owner Profile");
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
