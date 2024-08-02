582eeb13a0e1a52df96214f19a117cdc4f5b09dc
Ahmet Ege Göksal <aegoksal@hotmail.com>
2024-08-02 03:59:30 +0300

    Updated database, filtering and rating

--- View/SearchPage.java
+++ View/SearchPage.java
@@ -5,8 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -31,6 +34,8 @@ public class SearchPage implements Navigable{
     public double lowerBounds = 0;
     public double upperBounds = Double.MAX_VALUE;
 
+    @FXML
+    private Button filterButton;
 
     @FXML
     private FXMLLoader fxmlLoader2 = new FXMLLoader(getClass().getResource("RestaurantPage.fxml"));
@@ -69,20 +74,74 @@ public class SearchPage implements Navigable{
     private TextField textField7;
 
     @FXML
+    private ImageView rating1;
+    @FXML
+    private ImageView rating2;
+    @FXML
+    private ImageView rating3;
+    @FXML
+    private ImageView rating4;
+    @FXML
+    private ImageView rating5;
+    @FXML
+    private ImageView rating6;
+    @FXML
+    private ImageView rating7;
+    @FXML
+    private ImageView rating8;
+    @FXML
+    private ImageView rating9;
+    @FXML
+    private ImageView rating10;
+    @FXML
+    private ImageView rating11;
+    @FXML
+    private ImageView rating12;
+    @FXML
+    private ImageView rating13;
+    @FXML
+    private ImageView rating14;
+    @FXML
+    private ImageView rating15;
+    @FXML
+    private ImageView rating16;
+    @FXML
+    private ImageView rating17;
+    @FXML
+    private ImageView rating18;
+    @FXML
+    private ImageView rating19;
+    @FXML
+    private ImageView rating20;
+
+    @FXML
     private VBox filter;
     private boolean filterCheck = false;
+
+    @FXML
+    private Image filledStar = new Image("filledStar.png");
+    private Image emptyStar = new Image("emptyStar.png");
 
 
     @FXML
     void initialize() 
     {      
-        
+        rating1.setImage(filledStar);
+        rating6.setImage(filledStar);
+        rating11.setImage(filledStar);
+        rating12.setImage(filledStar);
+
         if(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().size() > 0)
         {
             text1.setText(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().get(0 + (4*pageNumber)).getRestaurantName());
             text2.setText(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().get(1 + (4*pageNumber)).getRestaurantName());
             text3.setText(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().get(2 + (4*pageNumber)).getRestaurantName());
             text4.setText(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().get(3 + (4*pageNumber)).getRestaurantName());
+
+            setStars1(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().get(0 + (4*pageNumber)));
+            setStars2(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().get(1 + (4*pageNumber)));
+            setStars3(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().get(2 + (4*pageNumber)));
+            setStars4(MealReelApplication.listOfUsers.getRestaurantOwnersFromListOfUsers().get(3 + (4*pageNumber)));
         }
     }
         
@@ -167,6 +226,7 @@ public class SearchPage implements Navigable{
     {
         filterCheck = !filterCheck;
         filter.setVisible(filterCheck);
+        filterButton.setDisable(!filterCheck);
     }
 
     public void radioButton1(ActionEvent event)
@@ -182,6 +242,12 @@ public class SearchPage implements Navigable{
         radioButton4.setSelected(sortByTotalDiscountOfItems);
 
         restaurantFilter.sortByAlphabeticalOrderOfRestaurantName();
+
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
+
     }
     public void radioButton2(ActionEvent event)
     {
@@ -196,6 +262,11 @@ public class SearchPage implements Navigable{
         radioButton4.setSelected(sortByTotalDiscountOfItems);
 
         restaurantFilter.sortByPopularity();
+
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
     }
     public void radioButton3(ActionEvent event)
     {
@@ -210,6 +281,11 @@ public class SearchPage implements Navigable{
         radioButton4.setSelected(sortByTotalDiscountOfItems);
 
         restaurantFilter.sortByRating();
+
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
     }
     public void radioButton4(ActionEvent event)
     {
@@ -224,27 +300,56 @@ public class SearchPage implements Navigable{
         radioButton4.setSelected(sortByTotalDiscountOfItems);
 
         restaurantFilter.sortByTotalDiscountOfItems();
+
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
     }
 
     public void textFile1(ActionEvent event)
     {
         restaurantFilter.includesGivenItem(textField1.getText());
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
     }
     public void textFile2(ActionEvent event)
     {
-        restaurantFilter.includesGivenItem(textField2.getText());
+        restaurantFilter.includesGivenItemType(textField2.getText());
+
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
     }
     public void textFile3(ActionEvent event)
     {
-        restaurantFilter.includesGivenItem(textField3.getText());
+        restaurantFilter.isFoundInGivenCity(textField3.getText());
+
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
     }
     public void textFile4(ActionEvent event)
     {
         restaurantFilter.includesGivenItem(textField4.getText());
+
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
     }
     public void textFile5(ActionEvent event)
     {
         restaurantFilter.includesGivenItem(textField5.getText());
+
+        text1.setText(restaurantFilter.getFilteredRestaurants().get(0 + (4*pageNumber)).getRestaurantName());
+        text2.setText(restaurantFilter.getFilteredRestaurants().get(1 + (4*pageNumber)).getRestaurantName());
+        text3.setText(restaurantFilter.getFilteredRestaurants().get(2 + (4*pageNumber)).getRestaurantName());
+        text4.setText(restaurantFilter.getFilteredRestaurants().get(3 + (4*pageNumber)).getRestaurantName());
     }
     public void textFile6(ActionEvent event)
     {
@@ -260,7 +365,87 @@ public class SearchPage implements Navigable{
         restaurantFilter.includesItemsBetweenSetPriceRange(Double.parseDouble(textField7.getText()), upperBounds);
     }
 
+    public void setStars1(RestaurantOwner restaurantOwner)
+    {
+        if(restaurantOwner.getRating() >= 2)
+        {
+            rating1.setImage(filledStar);
+            rating2.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() >= 3)
+        {
+            rating3.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() >= 4)
+        {
+            rating4.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() > 5)
+        {
+            rating5.setImage(filledStar);
+        }
+    }
 
+    public void setStars2(RestaurantOwner restaurantOwner)
+    {
+        if(restaurantOwner.getRating() >= 2)
+        {
+            rating6.setImage(filledStar);
+            rating7.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() >= 3)
+        {
+            rating8.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() >= 4)
+        {
+            rating9.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() > 5)
+        {
+            rating10.setImage(filledStar);
+        }
+    }
+    public void setStars3(RestaurantOwner restaurantOwner)
+    {
+        if(restaurantOwner.getRating() >= 2)
+        {
+            rating11.setImage(filledStar);
+            rating12.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() >= 3)
+        {
+            rating13.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() >= 4)
+        {
+            rating14.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() > 5)
+        {
+            rating16.setImage(filledStar);
+        }
+    }
+    public void setStars4(RestaurantOwner restaurantOwner)
+    {
+        if(restaurantOwner.getRating() >= 2)
+        {
+            rating16.setImage(filledStar);
+            rating17.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() >= 3)
+        {
+            rating18.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() >= 4)
+        {
+            rating19.setImage(filledStar);
+        }
+        if(restaurantOwner.getRating() > 5)
+        {
+            rating20.setImage(filledStar);
+        }
+    }
 
     @Override
     public void navigate() {
